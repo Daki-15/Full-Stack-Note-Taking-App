e38b93ff5d3cacb73800ccfaf1c6a3c6a5e442ba
Daki15 <darjanpaunovic@gmail.com>
2024-05-24 15:08:52 +0200

    adding data base

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html", boolean = False)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>LogOut</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fistName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 3 character!", category='error')
+        elif len(fistName) < 2:
+            flash("First Name must be greater than 1 character!", category='error')
+        elif password1 != password2:
+            flash("Passwords don't match!", category='error')
+        elif len(password1) < 7:
+            flash("Passwords must be at least 7 character!", category='error')
+        else:
+            #add user to database
+            new_user = User(email=email, fistName=fistName, password=password1)
+            flash("Account created!", category='success')
+
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -1,1 +1,18 @@
 # DATA BASE MODELS
+# We gona have 2 models, one for users and one for notes
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text_data = db.Column(db.String(10000))
+    date_of_note = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    notes = db.relationship('Note')
