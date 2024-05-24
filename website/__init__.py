b001eea4979fd30d9439a5771d6130fc405d01d9
Daki15 <darjanpaunovic@gmail.com>
2024-05-24 17:35:23 +0200

    backend functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db" 
@@ -18,10 +19,18 @@ def create_app():
     app.register_blueprint(auth, url_prefix= '/')
 
     from .models import User, Note
-
+    
     with app.app_context():
         create_database()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app=app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
 
 def create_database():
